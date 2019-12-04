2836cac30b04ca40ec72bc116548f0c9b65a30dd
alecdwm <alec@owls.io>
2019-12-04 16:40:57 +1000

    added 2019 day4 part2 solution

--- src/year_2019/day4.rs
+++ src/year_2019/day4.rs
@@ -24,7 +24,7 @@ pub fn part1() {
 
     let number_of_passwords = range
         .map(|integer| integer.to_string())
-        .filter(|password| test_password_against_facts(password.as_str()))
+        .filter(|password| part1_test_password_against_facts(password.as_str()))
         .count();
 
     println!(
@@ -33,18 +33,75 @@ pub fn part1() {
     );
 }
 
-fn test_password_against_facts(password: &str) -> bool {
+/// An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+///
+/// Given this additional criterion, but still ignoring the range rule, the following are now true:
+///
+///     112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+///     123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+///     111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+///
+/// How many different passwords within the range given in your puzzle input meet all of the criteria?
+pub fn part2() {
+    let input = crate::common::read_stdin_to_string();
+    let range = ops::RangeInclusive::<i64>::from_str(input.as_str());
+
+    let number_of_passwords = range
+        .map(|integer| integer.to_string())
+        .filter(|password| part2_test_password_against_facts(password.as_str()))
+        .count();
+
+    println!(
+        "The number of different passwords within the range given which meet all of the criteria: {}",
+        number_of_passwords
+    );
+}
+
+fn part1_test_password_against_facts(password: &str) -> bool {
     if password.len() != 6 {
         return false;
     }
 
     let chars: Vec<char> = password.chars().collect();
 
+    if chars.windows(2).any(|window| window[0] > window[1]) {
+        return false;
+    }
+
     if !chars.windows(2).any(|window| window[0] == window[1]) {
         return false;
     }
 
+    true
+}
+
+fn part2_test_password_against_facts(password: &str) -> bool {
+    if password.len() != 6 {
+        return false;
+    }
+
+    let chars: Vec<char> = password.chars().collect();
+
     if chars.windows(2).any(|window| window[0] > window[1]) {
+        return false;
+    }
+
+    let mut groups: Vec<Vec<char>> = vec![vec![*chars
+        .first()
+        .expect("Failed to get first character of password")]];
+
+    chars.windows(2).for_each(|window| {
+        if window[0] == window[1] {
+            groups
+                .last_mut()
+                .expect("Failed to get_mut last group")
+                .push(window[1]);
+        } else {
+            groups.push(vec![window[1]]);
+        }
+    });
+
+    if !groups.iter().any(|group| group.len() == 2) {
         return false;
     }
 
@@ -75,11 +132,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_password_factchecker() {
+    fn test_part1_password_factchecker() {
         let examples = [("112233", true), ("223450", false), ("123789", false)];
 
         for example in &examples {
-            assert_eq!(test_password_against_facts(example.0), example.1);
+            assert_eq!(part1_test_password_against_facts(example.0), example.1);
+        }
+    }
+
+    #[test]
+    fn test_part2_password_factchecker() {
+        let examples = [("112233", true), ("123444", false), ("111122", true)];
+
+        for example in &examples {
+            assert_eq!(part2_test_password_against_facts(example.0), example.1);
         }
     }
 }
